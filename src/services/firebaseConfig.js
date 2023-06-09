943c1d9fd3364c5c70ee7615ba37feeb6badbd7b
Pablo Leite <pabloleite03@gmail.com>
2023-06-09 09:21:40 -0300

    Add files via upload

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,11 +1,6 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged
-} from "firebase/auth";
-import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import "firebase/firestore";
 import "firebase/auth";
 
@@ -22,54 +17,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const createUserDocument = async (user, additionalData) => {
-  if (!user) return;
-
-  const userRef = doc(db, `users/${user.uid}`);
-  const snapshot = await getDoc(userRef);
-
-  if (!snapshot.exists()) {
-    const { displayName, email, uid } = user;
-    const { name } = additionalData;
-
-    try {
-      await setDoc(userRef, {
-        name,
-        displayName,
-        email,
-        uid,
-        createdAt: new Date(),
-      });
-    } catch (error) {
-      console.log("Erro ao criar usuário", error);
-    }
-  }
-
-  return getUserDocument(user.uid);
-};
-
-const registerUser = async (email, password, additionalData) => {
-  try {
-    const { user } = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    await createUserDocument(user, additionalData);
-    return user;
-  } catch (error) {
-    console.log("Erro ao registrar usuário", error);
-    throw error;
-  }  
-};
-
-const checkAuthentication = () => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      unsubscribe(); 
-      resolve(user); 
-    }, reject);
-  });
-};
-
-export { app as firebaseApp, auth, db, registerUser, checkAuthentication };
+export { app as firebaseApp, auth, db };
